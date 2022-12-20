6df7703765240f42f5f3f5635d6d0cbf38e1551f
danielkpobari <danielkpobari@gmail.com>
2022-12-20 22:30:33 +0100

    creation of boilerplate code

---
+++ src/main/java/com/daniel/droneServices/DroneServicesApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.droneServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/droneServices/DroneServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.droneServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
